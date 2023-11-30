c19a9623151c3d04a301cf98ffaea964ed623b6f
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2023-11-30 10:17:03 +0100

    Backup

---
+++ tp3/src/grammar/context.rs
@@ -0,0 +1,6 @@
+// Structure used to stock a variable and the value of this variable.
+#[derive(Clone)]
+pub struct Context {
+    pub variable: String,
+    pub value: f64,
+}
---
+++ tp3/src/postfix/topostfix.rs
@@ -0,0 +1,86 @@
+use crate::tokenize::token::*;
+
+// Give priority to operators
+fn give_priority(item: &String) -> i32 {
+    match item.as_str() {
+        "(" | ")" => {
+            return 0;
+        }
+        "+" | "-" => {
+            return 1;
+        }
+        "*" | "/" => {
+            return 2;
+        }
+        "^" => {
+            return 3;
+        }
+        _ => {
+            println!("Error !");
+            return -1;
+        }
+    }
+}
+
+// Allow to convert infix notation to postfix notation. It's like the algorithm given in the
+// instructions
+pub fn infix_to_postfix(mut input: Vec<Token>) -> Vec<Token> {
+    let mut result: Vec<Token> = vec![];
+    let mut stack: Vec<String> = Vec::new();
+    let mut priority = 0;
+    input.reverse();
+    while !input.is_empty() {
+        let item: Token = input.pop().unwrap();
+        match item.info {
+            Type::ReservedSymbol => {
+                if item.content.as_str() == "(" {
+                    priority = give_priority(&item.content);
+                    stack.push(item.content);
+                } else if item.content.as_str() == ")" {
+                    while !(stack
+                        .last()
+                        .unwrap_or(&"Error! None value".to_string())
+                        .as_str()
+                        == "(")
+                        && !stack.is_empty()
+                    {
+                        result.push(Token::create_token(
+                            Type::ReservedSymbol,
+                            stack.pop().unwrap_or("Error! None value".to_string()),
+                        ));
+                    }
+                    stack.pop().unwrap_or("Error! 3".to_owned());
+                    if !stack.is_empty() {
+                        priority = give_priority(&stack.last().unwrap_or(&"Error! 4".to_owned()));
+                    }
+                } else if give_priority(&item.content) > priority {
+                    priority = give_priority(&item.content);
+                    stack.push(item.content);
+                } else {
+                    while give_priority(&item.content) <= priority && !stack.is_empty() {
+                        result.push(Token::create_token(
+                            Type::ReservedSymbol,
+                            stack.pop().unwrap_or("Error! 5".to_owned()),
+                        ));
+                        if !stack.is_empty() {
+                            priority =
+                                give_priority(&stack.last().unwrap_or(&"Error! 6".to_owned()));
+                        }
+                    }
+                    priority = give_priority(&item.content);
+                    stack.push(item.content);
+                }
+            }
+            _ => {
+                result.push(item);
+            }
+        }
+    }
+    while !stack.is_empty() {
+        result.push(Token::create_token(
+            Type::ReservedSymbol,
+            stack.pop().unwrap(),
+        ));
+    }
+    result
+}
